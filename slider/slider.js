0a8f42daf2c6920f16b91441472b2d5e9904858c
Ross Wilson <rzzzwilson@gmail.com>
2016-09-18 18:35:39 +0700

    Add the experimental slider files

---
+++ slider/slider.js
@@ -0,0 +1,30 @@
+var bar, slider;
+
+function init(){
+	
+	bar = document.getElementById('bar');
+	slider = document.getElementById('slider');
+	info = document.getElementById('info');
+	bar.addEventListener('mousedown', startSlide, false);	
+	bar.addEventListener('mouseup', stopSlide, false);
+}
+
+function startSlide(event){
+	var set_perc = ((((event.clientX - bar.offsetLeft) / bar.offsetWidth)).toFixed(2));
+	info.innerHTML = 'start: ' + set_perc*100 + '%';	
+	bar.addEventListener('mousemove', moveSlide, false);	
+	slider.style.width = (set_perc * 100) + '%';	
+}
+
+function moveSlide(event){
+	var set_perc = ((((event.clientX - bar.offsetLeft) / bar.offsetWidth)).toFixed(2));
+	info.innerHTML = 'moving: ' + set_perc*100 + '%';
+	slider.style.width = (set_perc * 100) + '%';
+}
+
+function stopSlide(event){
+	var set_perc = ((((event.clientX - bar.offsetLeft) / bar.offsetWidth)).toFixed(2));
+	info.innerHTML = 'done: ' + set_perc*100 + '%';
+	bar.removeEventListener('mousemove', moveSlide, false);
+	slider.style.width = (set_perc * 100) + '%';
+}
