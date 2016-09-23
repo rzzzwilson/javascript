38c6957ef9e1aab555f63993f4cb5585f515b963
Ross Wilson <rzzzwilson@gmail.com>
2016-09-23 11:21:54 +0700

    Trying to turn into standalone UI widget

--- slider/slider.js
+++ slider/slider.js
@@ -1,22 +1,39 @@
 "use strict";
 
-var bar, slider, info;
+var display_div = null;
+var slider_div = null;
+var info = null;
 var leftButtonDown = false;
 
-function init() {
-	bar = document.getElementById('bar');
-	slider = document.getElementById('slider');
+var slider_div_name = 'slider';
+var slider_css = "#slider{width:0%;height:100%;background-color:red;top:0px;left:0px;position:absolute;cursor:pointer;};"
+
+function init(slider_div_name) {
+    // get slider <div> handle
+	slider_div = document.getElementById(slider_div_name);
+
+    // create CSS for the slider display and attach to slider_div
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = slider_css;
+    slider_div.appendChild(style);
+
+    // create slider display <div>
+    var display_div = document.createElement('div');
+    display_div.id = "slider";
+    slider_div.appendChild(display_div);
+
 	info = document.getElementById('info');
-	bar.addEventListener('mousedown', mouseDown, false);
-	bar.addEventListener('mousemove', mouseMove, false);
-	bar.addEventListener('mouseup', mouseUp, false);
+	slider_div.addEventListener('mousedown', mouseDown, false);
+	slider_div.addEventListener('mousemove', mouseMove, false);
+	slider_div.addEventListener('mouseup', mouseUp, false);
 }
 
 function mouseDown(event) {
     leftButtonDown = (event.button == 0);
 
     if (leftButtonDown) {
-    	var set_frac = ((((event.clientX - bar.offsetLeft) / bar.offsetWidth)).toFixed(2));
+    	var set_frac = ((((event.clientX - slider_div.offsetLeft) / slider_div.offsetWidth)).toFixed(2));
     	info.innerHTML = 'set: ' + Math.round(set_frac*100) + '%';
     	slider.style.width = (set_frac * 100) + '%';
     }
@@ -24,7 +41,7 @@ function mouseDown(event) {
 
 function mouseMove(event) {
 	if (leftButtonDown) {
-		var set_frac = ((((event.clientX - bar.offsetLeft) / bar.offsetWidth)).toFixed(2));
+		var set_frac = ((((event.clientX - slider_div.offsetLeft) / slider_div.offsetWidth)).toFixed(2));
 		info.innerHTML = 'dragging: ' + Math.round(set_frac*100) + '%';
 		slider.style.width = (set_frac * 100) + '%';
 	}
